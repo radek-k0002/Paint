af084dad104e8da71deccfa3553f8a0fa495bc49
Radosław Kisiel <Nexox@outlook.com>
2020-11-02 22:40:06 +0100

    Done

---
+++ Paint/BrushSettings.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Paint
+{
+    public partial class BrushSettings : Window
+    {
+        private SolidColorBrush color = Brushes.Black;
+        private int strokeThickness = 5;
+        private MainWindow window;
+
+        public BrushSettings(MainWindow window)
+        {
+            InitializeComponent();
+            this.window = window;
+        }
+
+        public SolidColorBrush Color { get { return color; } }
+        public int StrokeThickness { get { return strokeThickness; } }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            strokeThickness++;
+            ThicknessBox.Text = "" + strokeThickness;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            strokeThickness--;
+            ThicknessBox.Text = "" + strokeThickness;
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            ThicknessBox.Text = "" + strokeThickness;
+        }
+
+        private void Color_Click(object sender, RoutedEventArgs e)
+        {
+            color = (SolidColorBrush)((Button)sender).Background;
+        }
+
+        private void New_Click(object sender, RoutedEventArgs e)
+        {
+            window.MyCanvas.Children.Clear();
+        }
+    }
+}
---
+++ Paint/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Shapes;
+
+namespace Paint
+{
+    public partial class MainWindow : Window
+    {
+        private Polyline polyline;
+        private bool connectMode = false;
+        private bool forceConnectMode = false;
+        private BrushSettings brushSettings;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            brushSettings = new BrushSettings(this);
+        }
+
+        private void MyCanvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Draw(e);
+        }
+
+        private void MyCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                Draw(e);
+            }
+        }
+
+        private void Draw(MouseEventArgs e)
+        {
+            if (MyCanvas.Children.Count == 0 || !connectMode)
+            {
+                polyline = new Polyline();
+                polyline.Stroke = brushSettings.Color;
+                polyline.StrokeThickness = brushSettings.StrokeThickness;
+                MyCanvas.Children.Add(polyline);
+            }
+            polyline = (Polyline)MyCanvas.Children[MyCanvas.Children.Count - 1];
+            polyline.Stroke = brushSettings.Color;
+            polyline.StrokeThickness = brushSettings.StrokeThickness;
+
+            polyline.Points.Add(e.GetPosition(MyCanvas));
+            connectMode = true;
+        }
+
+        private void MyCanvas_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            forceConnectMode = !forceConnectMode;
+            MyCanvas_MouseLeftButtonUp(sender, e);
+        }
+
+        private void MyCanvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (!forceConnectMode)
+                connectMode = false;
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            brushSettings.Show();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            brushSettings.Close();
+        }
+    }
+}
